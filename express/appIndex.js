a7353953190de2536be29a249ef4e593d752a42e
wangziyi0526 <568771088@qq.com>
2020-03-24 23:12:49 +0800

    Ejs

--- express/appIndex.js
+++ express/appIndex.js
@@ -1,19 +1,24 @@
 let express = require('express');
 
 let app = express();
+
+app.set('view engine','ejs');// 视图引擎 view engine 
+
 // 根据用户请求的地址  返回对应的数据信息
 app.get('/',function(req,res){
   console.log(req.url);
-  res.send('This is home page!')
+  res.sendFile(__dirname+'/app.html');
 });
 app.get('/contact',function(req,res){
   console.log(req.url);
-  res.send("This is a contat page!")
+  res.sendFile(__dirname+'/aboutMe.html');
 });
 
 // 路由参数   
 app.get('/profile/:id',function(req,res){
-  res.send('您所访问的路径参数为 : '+ req.params.id); // 拿到 这个输入的路由参数
+  // res.sendFile('您所访问的路径参数为 : '+ req.params.id); // 拿到 这个输入的路由参数
+  let data = [{name:'wangziyi',age:28},{name:"kobe Bryant",age:41}];
+  res.render('profile',{title:'This is Ejs page!',name:'wangziyi',data:data});
 })
 
 app.listen(8818)
